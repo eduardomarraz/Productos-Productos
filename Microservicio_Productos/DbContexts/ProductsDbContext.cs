8176901e5a3df5c8cca2ff09bbd0396ba1d50528
Misael <misaelenrique.rodriguez@tajamar365.com>
2024-01-24 18:41:34 +0100

    Base de datos creada

---
+++ Microservicio_Productos/DbContexts/ProductsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microservicio_Productos.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+namespace Microservicio_Productos.DbContexts
+{
+    public class ProductsDbContext : DbContext
+    {
+        public ProductsDbContext(DbContextOptions<ProductsDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
