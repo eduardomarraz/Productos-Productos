b33026b24ba7c424ad15f54258ef8530ca70727d
Óscar Diago Fernández <oscar.diago@tajamar365.com>
2024-01-24 17:25:51 +0100

    Microservicio iniciado

---
+++ Microservicio_Productos/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Microservicio_Productos.Models
+{
+    public class Category
+    {
+        public Guid CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public List<Product> Products { get; set;}
+
+    }
+}
---
+++ Microservicio_Productos/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Microservicio_Productos.Models
+{
+    public class Product
+    {
+        public Guid ProductId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Guid CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
